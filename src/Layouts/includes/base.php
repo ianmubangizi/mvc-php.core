6fe6cd170ab74420262686ae29b1dec3ca2b4273
Ian Mubangizi <io@ianmubangizi.com>
2020-10-28 13:25:30 +0200

    fix static

--- src/Layouts/includes/base.php
+++ src/Layouts/includes/base.php
@@ -7,11 +7,8 @@
     <link rel="icon" type="image/svg" sizes="32x32" href="/public/img/logo-icon-gold.svg">
     <link rel="icon" type="image/svg" sizes="16x16" href="/public/img/logo-icon-gold.svg">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/css/simple-bar.min.css">
-    <link rel="stylesheet" href="/public/css/tiny-slider.css">
-    <link rel="stylesheet" href="/public/css/ui-slider.min.css">
-    <link rel="stylesheet" href="/public/css/keyframes.css" />
-    <link rel="stylesheet" href="/public/css/theme.min.css">
+    <link href="/public/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/public/css/main.css" rel="stylesheet">
     <title><?php echo get_title() ?></title>
 </head>
 
@@ -21,15 +18,8 @@
     include_once($app->page->view);
     include_once('footer.php');
     ?>
-    <script src="/public/js/jquery.slim.min.js"></script>
+    <script src="/public/js/popper.min.js"></script>
     <script src="/public/js/bootstrap.bundle.min.js"></script>
-    <script src="/public/js/file-input.min.js"></script>
-    <script src="/public/js/simple-bar.min.js"></script>
-    <script src="/public/js/tiny-slider.js"></script>
-    <script src="/public/js/ui-slider.min.js"></script>
-    <script src="/public/js/drift.min.js"></script>
-    <script src="/public/js/smooth-scroll.polyfills.min.js"></script>
-    <script src="/public/js/functions.min.js"></script>
 </body>
 
 </html>
--- src/Layouts/includes/footer.php
+++ src/Layouts/includes/footer.php
@@ -1,8 +1,3 @@
-    <footer>
-        <p>Footer</p>
-    </footer>
-    <script src="/public/js/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="/public/js/bootstrap.bundle.min.js" integrity="sha384-DBjhmceckmzwrnMMrjI7BvG2FmRuxQVaTfFYHgfnrdfqMhxKt445b7j3KBQLolRl" crossorigin="anonymous"></script>
-    </body>
-
-    </html>
+<footer class="bg-dark pt-5">
+    <h1>Footer</h1>
+</footer>
